b93e4b41beea198d7c9daa67047af4c6afafdbf6
Adam Mazur <adam.mazur1323@interia.pl>
2023-05-01 22:11:41 +0200

    xyz

--- medicinal_chemistry/powershell_converter.py
+++ medicinal_chemistry/powershell_converter.py
@@ -1,10 +1,10 @@
 import os
 
-path = r"C:\Users\OPERATOR\Desktop\Studia\SKN organ\Dokowanie\photoswitche\QM4"
+path = r"C:\Users\..."
 dir_list = os.listdir(path)
 for x in  dir_list:
         if x.endswith('mol2'):
-                with open (f"C:\\Users\\OPERATOR\\Desktop\\Studia\\SKN organ\\Dokowanie\\photoswitche\\QM4\\{x}") as plik:
-                    tekst = plik.read()
+                with open (f"C:\\Users\\...\\{x}") as file:
+                    text = file.read()
                     print(f'obabel {x}', f' -O {x}'.replace('mol2', 'pdbqt'))
 print('Done')
